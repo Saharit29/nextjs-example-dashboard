e286d34d3151748516c44197f1ad1e985799dba1
Saharit Potithongdee <89732940+Saharit29@users.noreply.github.com>
2024-07-01 13:49:15 +0700

    add pagination and customer table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,35 @@
 import { Metadata } from 'next';
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import {  fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customer = await fetchFilteredCustomers(query);
+
+return (
+  <div className="w-full">
+     <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Table customers={customer}></Table>
+    </Suspense>
+  </div>
+);
 }
